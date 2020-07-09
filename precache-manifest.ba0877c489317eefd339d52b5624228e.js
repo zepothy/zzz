b7d10c6bea99838274bdd6508287cee45b9972b5
jen-w <10570219+jen-w@users.noreply.github.com>
2020-07-08 23:32:58 -0700

    haha nice
    6d8adef8afe7181bf180fc36911d572c208a6992

---
+++ precache-manifest.ba0877c489317eefd339d52b5624228e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d613cdecd209d06990d81849c1366d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e342e57adcd9a7170ba5",
+    "url": "/static/css/main.108e4cd8.chunk.css"
+  },
+  {
+    "revision": "90adca1db5ffaa5a34ee",
+    "url": "/static/js/2.103d0c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.103d0c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e342e57adcd9a7170ba5",
+    "url": "/static/js/main.eab64599.chunk.js"
+  },
+  {
+    "revision": "fa9e55b41a00fc59679a",
+    "url": "/static/js/runtime-main.ab439063.js"
+  }
+]);
